268f72aa5df5098e0e5a1c50964cff2aa6ddcbb3
fkurusu7 <coding.fcv@gmail.com>
2025-04-28 00:40:20 -0500

    add tooltips

--- smallies/src/minis/tooltip/TooltipApp.jsx
+++ smallies/src/minis/tooltip/TooltipApp.jsx
@@ -4,16 +4,43 @@ import { useState } from "react";
 const TooltipApp = () => {
   return (
     <div className="tooltip__container">
-      <Tooltip info={"Yoir rder will be placed after the payment"}>
+      <Tooltip
+        info={"Your order will be placed LEFT after the payment"}
+        position="left"
+      >
         <button type="button" className="btn">
-          Pay Now
+          Left
+        </button>
+      </Tooltip>
+      <Tooltip
+        info={"Your order will be placed after the payment"}
+        position="top"
+      >
+        <button type="button" className="btn">
+          Top
+        </button>
+      </Tooltip>
+      <Tooltip
+        info={"Your order will be placed BOTTOM after the payment"}
+        position="bottom"
+      >
+        <button type="button" className="btn">
+          Bottom
+        </button>
+      </Tooltip>
+      <Tooltip
+        info={"Your order will be placed RIGHT after the payment"}
+        position="right"
+      >
+        <button type="button" className="btn">
+          Right
         </button>
       </Tooltip>
     </div>
   );
 };
 
-const Tooltip = ({ info, children }) => {
+const Tooltip = ({ info, children, position = "top" }) => {
   // State to manage tooltip visibility
   const [showTooltip, setShowTooltip] = useState(false);
 
@@ -24,12 +51,17 @@ const Tooltip = ({ info, children }) => {
       onMouseLeave={() => setShowTooltip(false)}
     >
       {children}
-      <div className={`tooltip__info ${showTooltip && "tooltip__info--open"}`}>
+      <div
+        className={`tooltip__info tooltip__info--${position} 
+          ${showTooltip ? "tooltip__info--open" : ""} 
+          ${showTooltip ? `tooltip__info--open-${position}` : ""}`}
+      >
         {info}
         {/* Arrow indicator */}
-        <div className="arrow"></div>
+        <div className={`arrow arrow--${position}`}></div>
       </div>
     </div>
   );
 };
+
 export default TooltipApp;
