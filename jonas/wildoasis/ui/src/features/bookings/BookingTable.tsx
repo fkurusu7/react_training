6f207ade71e1362025ce535595c9039f64522749
fkurusu7 <coding.fcv+github@gmail.com>
2025-07-10 23:36:56 -0600

    add custom hook to get bookings

--- jonas/wildoasis/ui/src/features/bookings/BookingTable.tsx
+++ jonas/wildoasis/ui/src/features/bookings/BookingTable.tsx
@@ -1,13 +1,17 @@
-import BookingRow from "./BookingRow";
-import Table from "../../ui/Table";
-import Menus from "../../ui/Menus";
+import Menus from '../../ui/Menus';
+import Spinner from '../../ui/Spinner';
+import Table from '../../ui/Table';
+import { useBookings } from './useGetBookings';
 
 function BookingTable() {
-  const bookings = [];
+  const { data, isPending, error } = useBookings();
+  console.log('data', data);
+  console.log('error', error);
+  if (isPending) return <Spinner />;
 
   return (
     <Menus>
-      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+      <Table columns='0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem'>
         <Table.Header>
           <div>Cabin</div>
           <div>Guest</div>
@@ -17,12 +21,12 @@ function BookingTable() {
           <div></div>
         </Table.Header>
 
-        <Table.Body
+        {/* <Table.Body
           data={bookings}
           render={(booking) => (
             <BookingRow key={booking.id} booking={booking} />
           )}
-        />
+        /> */}
       </Table>
     </Menus>
   );
